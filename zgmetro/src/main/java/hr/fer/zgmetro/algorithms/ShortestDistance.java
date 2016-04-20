36282d41843c7231b0eaa38ad0d6acfc54de10e3
icoric4 <ivancoric@Ivans-MBP.lan>
2016-04-21 00:10:43 +0200

    shortestDistance napravljen i testiran

--- zgmetro/src/main/java/hr/fer/zgmetro/algorithms/ShortestDistance.java
+++ zgmetro/src/main/java/hr/fer/zgmetro/algorithms/ShortestDistance.java
@@ -1,11 +1,68 @@
 package hr.fer.zgmetro.algorithms;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 import hr.fer.zgmetro.model.Graph;
+import hr.fer.zgmetro.model.Node;
+import hr.fer.zgmetro.model.loader.FileLoader;
 
 public class ShortestDistance {
 
-	//mora raditi i za start == end !!!!!!!!!!!!!!!!!!!!!!!
 	public static Integer calculate(Graph graph, String start, String end)	{
-		return 0;
+		
+		TreeMap<Integer, Node> open = new TreeMap<>();
+		open.put(0, graph.getNodeByName(start));
+		int done;
+		
+		if(start.equals(end))
+			done = -1;
+		else 
+			done = 0;
+		
+		Set<Node> visited = new HashSet<>();
+		
+		while (!open.isEmpty())	{
+			int dist = open.firstKey();
+			Node node = open.get(open.firstKey());
+			open.remove(open.firstKey());
+			
+			if(done != -1)
+				visited.add(node);
+			if(node.getName().equals(end))	{
+				++done;
+			}
+			
+			if(done == 1)
+				return dist;
+			
+			for(Entry<Node, Integer> e : node.getSuccessors().entrySet())	{
+				if(!visited.contains(e.getKey()))
+					open.put(e.getValue()+dist,e.getKey());
+			}
+		}		
+		return -1;
 	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			FileLoader fl = new FileLoader("metro.txt");
+			Graph graph = fl.load();
+			System.out.println("SADFASD");
+			System.out.println(calculate(graph, "DUBRAVA", "DUBRAVA"));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
 }
